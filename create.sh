8fde418d7bcf230be924d3cbcaee1787eaacc523
Guillaume Ballet <gballet@gmail.com>
2019-09-09 16:36:37 +0200

    First, uncleaned version of the repo

---
+++ create.sh
@@ -0,0 +1,15 @@
+#!/usr/bin/env bash
+if [[ $# == 0 ]]
+then
+	echo "Need an archive name"
+	exit 1
+fi
+
+sudo rm -rf chrootdir
+mkdir chrootdir
+sudo tar xfJ $1 -C chrootdir
+
+sudo cp `which qemu-aarch64-static` chrootdir/usr/bin
+sudo rsync -a $GOPATH/src/github.com/ethereum chrootdir/go/src/github.com/ethereum/go-ethereum
+
+sudo chroot chrootdir qemu-aarch64-static /bin/bash
