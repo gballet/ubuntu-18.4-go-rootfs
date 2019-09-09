259b945c7800046c709fb835779aa33c9f199164
Guillaume Ballet <gballet@gmail.com>
2019-09-09 17:39:03 +0200

    Fix code copy, not fully working

--- create.sh
+++ create.sh
@@ -10,6 +10,7 @@ mkdir chrootdir
 sudo tar xfJ $1 -C chrootdir
 
 sudo cp `which qemu-aarch64-static` chrootdir/usr/bin
+sudo mkdir -p chrootdir/go/src/github.com/ethereum/go-ethereum
 sudo rsync -a $GOPATH/src/github.com/ethereum chrootdir/go/src/github.com/ethereum/go-ethereum
 
 sudo chroot chrootdir qemu-aarch64-static /bin/bash
